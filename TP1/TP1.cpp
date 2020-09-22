01c2da593a5e8b02a875519a97c152b39cbda9c7
Apakoh <quentin.bartschs@gmail.com>
2020-09-22 03:49:03 +0200

    Random stuff

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -3,17 +3,27 @@
 #include <glm/gtx/intersect.hpp>
 #include <sfml/graphics.hpp>
 #include <sfml/graphics/image.hpp>
+#include <math.h>
 
 int main()
 {
   unsigned int height = 800;
   unsigned int width = 800;
   Ray r;
-  glm::vec3 out;
+  glm::vec3 intersection_position;
+  glm::vec3 intersection_normal;
+
 
   Sphere s;
   s.center = glm::vec3(width/2, height/2, 0);
   s.radius = 100.0;
+
+  //Light
+  glm::vec3 lamp = glm::vec3(100, 100, 100);
+  glm::vec3 l_e = glm::vec3(255, 0, 0);
+
+  glm::vec3 sphere_color = glm::vec3(255, 255, 255);
+
   sf::RenderWindow window(sf::VideoMode(width, height), "Main Window");
 
   sf::Image img;
@@ -25,9 +35,36 @@ int main()
     {
       r.origin = glm::vec3(x, y, 0);
       r.direction = glm::vec3(0,0,1);
-      if(glm::intersectRaySphere(r.origin, r.direction, s.center, s.radius, out, out))
+
+      if(glm::intersectRaySphere(r.origin, r.direction, s.center, s.radius, intersection_position, intersection_normal))
       {
-        img.setPixel(x, y, sf::Color(255,255,255));
+        glm::vec3 lamp_direction = lamp - intersection_position;
+        if(!glm::intersectRaySphere(intersection_position, lamp_direction, s.center, s.radius, intersection_position, intersection_normal))
+        {
+          // Calcul of D
+          float distance_carre = lamp_direction.x * lamp_direction.x  + lamp_direction.y * lamp_direction.y  + lamp_direction.z * lamp_direction.z;
+          //std::cout << distance_carre << std::endl;
+
+          // Calcul for color
+          // glm::vec3 l_e_and_object_color = (l_e * sphere_color);
+          // l_e_and_object_color = glm::vec3(l_e_and_object_color.x/255, l_e_and_object_color.y/255, l_e_and_object_color.z/255);
+
+          glm::vec3 lamp_to_intersect = glm::normalize(lamp_direction) * glm::normalize(lamp_direction);
+
+          // Calcul for cos theta
+          float cos_theta = glm::abs(glm::dot(glm::normalize(r.direction),glm::normalize(lamp_direction)));
+          //std::cout << cos_theta << std::endl;
+
+          // Final Fantasy Calcul : A Real Reborn XII
+          glm::vec3 color_temp = sphere_color * lamp_to_intersect * (cos_theta / (float)M_PI);
+          sf::Color color = sf::Color(color_temp.x, color_temp.y, color_temp.z);
+          img.setPixel(x, y, color);
+          img.setPixel(x, y, sf::Color(0, 255, 255));
+        }
+        else
+        {
+          img.setPixel(x, y, sf::Color(sphere_color.x, sphere_color.y, sphere_color.z));
+        }
       }
       else
       {
@@ -35,6 +72,13 @@ int main()
       }
     }
   }
+
+  // 0 ------------> x
+  // |
+  // |
+  // |
+  // V
+  // y
 
   sf::Texture texture;
   texture.loadFromImage(img);
--- TP1/tp1_unit_test.cpp
+++ TP1/tp1_unit_test.cpp
@@ -1,17 +1,28 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-
-TEST(module_name, test_name) a
+int Factorial (int x, int result = 1)
 {
-    std::cout << "Test" << std::endl;
-    // Google Test will also provide macros for assertions.
-    ASSERT_EQ(1+1, 2);
+  if(x == 1)
+  {
+    return result;
+  }
+  else
+  {
+    return Factorial(x - 1, x * result);
+  }
 }
 
-int main()
+TEST(FactorialTest, Negative)
+{
+    //ASSERT_EQ(1+1, 2);
+    EXPECT_EQ(1, Factorial(-5));
+    EXPECT_EQ(1, Factorial(-1));
+    EXPECT_EQ(Factorial(-10), 0);
+}
+
+int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-
     return RUN_ALL_TESTS();
 }
